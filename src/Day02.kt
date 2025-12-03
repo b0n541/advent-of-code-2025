7efcf4af9f4f1e0655e9dc5825921332a5cd4abd
Jan Schäfer <2326303+b0n541@users.noreply.github.com>
2025-12-03 15:47:40 +0100

    Add solution for day 2

---
+++ src/Day02.kt
@@ -0,0 +1,100 @@
+import org.assertj.core.api.Assertions.assertThat
+
+fun main() {
+    fun part1(input: List<String>): Long {
+
+        var invalidIdSum = 0L
+
+        input[0].split(",")
+            .map { range ->
+                LongRange(
+                    range.split("-")[0].toLong(),
+                    range.split("-")[1].toLong()
+                )
+            }
+            .forEach { range ->
+                run {
+                    println("Checking IDs $range")
+
+                    range.forEach { productId ->
+                        run {
+                            if (isInvalidProductId(productId)) {
+                                println("Invalid ID: $productId")
+                                invalidIdSum += productId
+                            }
+                        }
+                    }
+                }
+            }
+
+        return invalidIdSum
+    }
+
+    fun part2(input: List<String>): Long {
+
+        var invalidIdSum = 0L
+
+        input[0].split(",")
+            .map { range ->
+                LongRange(
+                    range.split("-")[0].toLong(),
+                    range.split("-")[1].toLong()
+                )
+            }
+            .forEach { range ->
+                run {
+                    println("Checking IDs $range")
+
+                    range.forEach { productId ->
+                        run {
+                            if (isInvalidProductId2(productId)) {
+                                println("Invalid ID: $productId")
+                                invalidIdSum += productId
+                            }
+                        }
+                    }
+                }
+            }
+
+        return invalidIdSum
+    }
+
+    // Test if implementation meets criteria from the description, like:
+    //check(part1(listOf("test_input")) == 1)
+
+    // Or read a large test input from the `src/Day01_test.txt` file:
+    val testInput = readInput("Day02_test")
+    assertThat(part1(testInput)).isEqualTo(1227775554)
+
+    // Read the input from the `src/Day01.txt` file.
+    val input = readInput("Day02")
+    part1(input).println()
+
+    assertThat(part2(testInput)).isEqualTo(4174379265)
+    part2(input).println()
+}
+
+fun isInvalidProductId(id: Long): Boolean {
+
+    val idString = id.toString()
+
+    return idString.take(idString.length / 2) == idString.substring(idString.length / 2)
+}
+
+fun isInvalidProductId2(id: Long): Boolean {
+
+    val idString = id.toString()
+
+    for (chunkSize in 1..idString.length / 2) {
+        val chunk = idString.take(chunkSize)
+        var repetitionCount = 2
+        while (chunk.length * repetitionCount <= idString.length) {
+            if (idString == chunk.repeat(repetitionCount)) {
+                return true
+            }
+            repetitionCount++
+        }
+    }
+
+    return false
+}
